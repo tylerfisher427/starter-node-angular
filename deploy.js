6634e55cacb040191a4700d053c95f2c7ab292ab
Tyler Fisher <tylerrayfisher@yahoo.com>
2020-12-10 17:17:59 -0600

    updated deploy.js and travis yml

--- deploy.js
+++ deploy.js
@@ -2,8 +2,10 @@ var cmd = require('node-cmd');
 var path, node_ssh, ssh, fs;
 fs = require('fs');
 path = require('path');
-node_ssh = require('node-ssh');
-ssh = new node_ssh();
+// node_ssh = require('node-ssh');
+// ssh = new node_ssh();
+const {NodeSSH} = require('node-ssh')
+ssh = new NodeSSH();
 
 // the method that starts the deployment process
 function main() {
@@ -22,8 +24,8 @@ function installPM2() {
 // transfers local project to the remote server
 function transferProjectToRemote(failed, successful) {
   return ssh.putDirectory(
-    '..riot-express-todo-list',
-    '/home/ubuntu/riot-express-todo-list-temp',
+    '..starter-node-angular',
+    '/home/ubuntu/starter-node-angular-temp',
     {
       recursive: true,
       concurrency: 1,
@@ -49,7 +51,7 @@ function transferProjectToRemote(failed, successful) {
 // creates a temporary folder on the remote server
 function createRemoteTempFolder() {
   return ssh.execCommand(
-    'rm -rf riot-express-todo-list-temp && mkdir riot-express-todo-list-temp', {
+    'rm -rf starter-node-angular-temp && mkdir starter-node-angular-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -65,7 +67,7 @@ function stopRemoteServices() {
 // updates the project source on the server
 function updateRemoteApp() {
   return ssh.execCommand(
-    'mkdir riot-express-todo-list && cp -r riot-express-todo-list-temp/* riot-express-todo-list/ && rm -rf riot-express-todo-list-temp', {
+    'mkdir starter-node-angular && cp -r starter-node-angular-temp/* starter-node-angular/ && rm -rf starter-node-angular-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -73,7 +75,7 @@ function updateRemoteApp() {
 // restart mongodb and node services on the remote server
 function restartRemoteServices() {
   return ssh.execCommand(
-    'cd riot-express-todo-list && sudo service mongod start && pm2 start app.js', {
+    'cd starter-node-angular && sudo service mongod start && pm2 start app.js', {
       cwd: '/home/ubuntu'
   });
 }
@@ -95,7 +97,7 @@ function sshConnect() {
       return installPM2();
     })
     .then(function() {
-      console.log('Creating `riot-express-todo-list-temp` folder.');
+      console.log('Creating `starter-node-angular-temp` folder.');
       return createRemoteTempFolder();
     })
     .then(function(result) {
